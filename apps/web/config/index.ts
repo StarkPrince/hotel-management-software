3d16e5de47f32e04106b2cea3d3e837e434ee9a5
Starkprince <games.princeraj@gmail.com>
2024-12-15 11:32:25 +0530

    made the prisma client singleton

--- apps/web/config/index.ts
+++
@@ -1,7 +0,0 @@
-// Only include dotenv in server-side code
-if (typeof window === "undefined") {
-  require("dotenv").config();
-}
-
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "https://localhost:4000/api";
--- prisma/migrations/20241214230036_turborepo/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "_AmenityToRoom" ADD CONSTRAINT "_AmenityToRoom_AB_pkey" PRIMARY KEY ("A", "B");
-
--- DropIndex
-DROP INDEX "_AmenityToRoom_AB_unique";
